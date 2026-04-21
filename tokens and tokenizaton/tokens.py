bea75ad97d1014ca3d4b95cab97c092f2bc226cd
Venky <venkateshvanjarapu1622@gmail.com>
2026-04-21 16:59:21 +0530

    tokenization

--- tokens and tokenizaton/tokens.py
+++ tokens and tokenizaton/tokens.py
@@ -4,3 +4,6 @@ tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo")
 text = "who is the pm of india"
 tokens = tokenizer.encode(text)
 print(tokens)
+result_tokens = [1820, 9012, 315, 28811, 374, 1491, 35973]
+result_text = tokenizer.decode(result_tokens)
+print(result_text)
